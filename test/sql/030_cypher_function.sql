ec45afeed0a5f0606c6fcbb03f430409cac2b5b5
Alastair McKinley <a.mckinley@analyticsengines.com>
2019-11-02 18:51:30 +0000

    WIP

--- test/sql/030_cypher_function.sql
+++ test/sql/030_cypher_function.sql
@@ -4,4 +4,4 @@ SELECT *  FROM cypher('MATCH (m:Movie)  WHERE m.title CONTAINS "The Matrix" RETU
 SELECT *  FROM cypher('MATCH (m:Movie)  WHERE m.title CONTAINS $search RETURN m ORDER BY id(m) DESC','{''search'':''Matrix''}');
 SELECT *  FROM cypher('MATCH (m:Movie)  WHERE m.title CONTAINS $search RETURN m ORDER BY id(m) DESC','{"search":"Matrix"}');
 SELECT name, born  FROM cypher('MATCH (p:Person)-[r:ACTED_IN]->(m:Movie) WHERE m.title=$movie RETURN p.name AS name, p.born AS born ORDER BY born DESC','{"movie":"The Matrix"}') , json_to_record(cypher) as x(name varchar, born smallint);
-select * from cypher('create (a {name : "test"}),(b {name : "thing"}),(c {name : "other"}) return *;');
+select json_object_agg(key,value::jsonb-'id') as result from cypher('create (a {name : "test"}),(b {name : "thing"}),(c {name : "other"}) return *;') c, lateral json_each(c);
